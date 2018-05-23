e83b892c5493ae03b8d39cbf52ddfa0452d71079
SuperHenry2333 <931851825@qq.com>
2018-05-23 10:18:18 +0800

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# pwnable.kr
+## learn pwn
+## dn用于自动生成题目文件夹，下载题目，并生成登录脚本和解题脚本模板，用法：./dn challenge_name
+## 每个题目login.sh用于登录，solve.py用于解题
---
+++ commit.sh
@@ -0,0 +1,6 @@
+# git init
+git add --all
+git commit -m $1
+# git remote add origin git@github.com:michaelliao/learngit.git
+# git remote add origin https://github.com/SuperHenry2333/pwnable_tw.git
+git push -u git@github.com:SuperHenry2333/pwnable_kr.git master
